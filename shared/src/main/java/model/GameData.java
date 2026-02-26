405c83329146324f82d5c9045b6f4327826ebe84
Kassidy <kassidywhite514@gmail.com>
2026-02-26 14:13:16 -0700

    all files setup

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,10 @@
+package model;
+import chess.ChessGame;
+
+public class GameData {
+    int gameID;
+    String whiteUsername;
+    String blackUsername;
+    String gameName;
+    ChessGame game;
+}
