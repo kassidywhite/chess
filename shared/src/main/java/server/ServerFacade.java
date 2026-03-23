0b4627f6e49f7f7b903f3d13bcc36872dcb52abe
Kassidy <kassidywhite514@gmail.com>
2026-03-23 12:46:46 -0600

    created ResponseException class

---
+++ shared/src/main/java/server/ServerFacade.java
@@ -0,0 +1,69 @@
+package server;
+
+import com.google.gson.Gson;
+import model.request.JoinGameRequest;
+import model.request.LoginRequest;
+import model.request.RegisterRequest;
+import model.result.JoinGameResult;
+import model.result.LoginResult;
+import model.result.RegisterResult;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public RegisterResult register(RegisterRequest request) throws Exception {
+        var path = "/user";
+        this.makeRequest("POST", path, request, RegisterResult.class);
+    }
+
+//    public LoginResult login(LoginRequest request) {...}
+//
+//    public JoinGameResult register(JoinGameRequest request) {...}
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws Exception {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+        }
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws Exception {
+        if(request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, Exception {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new Exception();
+        }
+    }
+
+    private boolean isSuccessful(int status) {return status / 100 == 2};
+}
+
